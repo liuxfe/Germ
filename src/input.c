f61856226df46612f76a3e9c4e54f8a398ee8c9b
ToHack <tohack@foxmail.com>
2017-12-13 15:21:31 +0800

    r4

--- src/input.c
+++ src/input.c
@@ -6,11 +6,12 @@ typedef struct _buffer{
 	int     nalloc;
 	int     nchars;
 	char*   data;
-} Buffer;
+} buffer;
 
-void _insertCharToBuffer(Buffer* buf, char ch){
+void insertCharToBuffer(buffer* buf, char ch){
 	const int FB_GROWSIZE = 1024;
 	char* tmp;
+
 	if(buf->nchars+1 >= buf->nalloc){
 		tmp = Xmalloc(buf->nalloc + FB_GROWSIZE, __FILE__, __LINE__);
 		if(buf->data){
@@ -26,13 +27,13 @@ void _insertCharToBuffer(Buffer* buf, char ch){
 char* LoadFile(char* filename){
 	int  ch;
 
-	Buffer buf = {0, 0, NULL};
+	buffer buf = {0, 0, NULL};
 	FILE* file = Xfopen(filename, "r", __FILE__, __LINE__);
 
 	for(ch=fgetc(file); ch != EOF; ch=fgetc(file)){
-		_insertCharToBuffer(&buf,(char)ch);
+		insertCharToBuffer(&buf,(char)ch);
 	}
-	_insertCharToBuffer(&buf,'\0');
+	insertCharToBuffer(&buf,'\0');
 
 	Xfclose(file, __FILE__, __LINE__);
 
