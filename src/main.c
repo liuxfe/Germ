2f006a6d717cde3c715c5fc59f4beb1879ec1bf0
tohack <tohack@foxmail.com>
2017-12-16 02:38:19 -0500

    save

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 2017 Tohack<tohack@foxmail.com>. All Rights Reserved. */
 
 #include "main.h"
+#include <assert.h>
 
 int doComplie(char* file){
 	ParseFile(file);
@@ -11,6 +12,19 @@ int doComplie(char* file){
 int main(int argc, char* argv[]){
 	FILE* out = stdout;
 
+	assert(sizeof(int8)  ==1);
+	assert(sizeof(int16) ==2);
+	assert(sizeof(int32) ==4);
+	assert(sizeof(int64) ==8);
+	assert(sizeof(uint8) ==1);
+	assert(sizeof(uint16)==2);
+	assert(sizeof(uint32)==4);
+	assert(sizeof(uint64)==8);
+	assert(sizeof(bool)  ==1);
+	assert(sizeof(void*) ==8);
+	//assert(sizeof(int)   ==8);
+	//assert(sizeof(uint)  ==8);
+	
 	if(argc != 2){
 		printf("Usage: germ <source-file>\n");
 		exit(-1);
